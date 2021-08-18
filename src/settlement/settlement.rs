70003386a15974e945a872cd12a12da316b5696b
msk- <mattkingston@gmail.com>
2021-08-18 12:16:42 +0100

    Add ts types to settlement window stuff

--- src/settlement/settlement.rs
+++ src/settlement/settlement.rs
@@ -1,6 +1,10 @@
 use serde::{Serialize, Deserialize};
 
+#[cfg(feature = "typescript_types")]
+use ts_rs::TS;
+
 // TODO: is this actually u64? It's likely whatever type MySQL uses as an auto-incrementing
 // integer.
+#[cfg_attr(feature = "typescript_types", derive(TS))]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SettlementId(u64);
